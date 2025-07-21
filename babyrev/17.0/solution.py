864d1c5ef8bff555bacaf99a10cef134f194d881
tamir hayne <tamir.hayne@cellebrite.com>
2025-07-21 23:44:15 +0300

    babyrev

---
+++ babyrev/17.0/solution.py
@@ -0,0 +1,28 @@
+import sys
+import subprocess
+from pathlib import Path
+
+solution = b"\xf5\x1c\x1c\x36\x5f\x84"
+
+def solve(exec: Path):
+    if exec.exists():
+        proc = subprocess.Popen([str(exec)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        proc.stdin.write(solution)
+        proc.stdin.close()
+
+        result = proc.stdout.read()
+        print(result.decode('utf-8'))
+    else:
+        print(f"no exec: {exec}")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python solution.py <path_to_exec>")
+        sys.exit(1)
+    solve(Path(sys.argv[1]))
+
+if __name__ == "__main__":
+    main()
+    
